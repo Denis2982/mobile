cba975ad8c0f1c5919315d0a349ade55b0425b03
Denis2982 <denisdenis2982@gmail.com>
2023-04-11 17:46:15 +0300

    added

--- src/app/pages/profile/profile.module.ts
+++ src/app/pages/profile/profile.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfileComponent } from './profile.component';
 import { RouterModule, Routes } from '@angular/router';
+import { UserComponent } from './user/user.component';
+import { ReportsComponent } from './reports/reports.component';
+import { SideComponent } from './side/side.component';
 
 const routes: Routes = [
   { path: '', component: ProfileComponent }
@@ -9,7 +12,10 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [
-    ProfileComponent
+    ProfileComponent,
+    UserComponent,
+    ReportsComponent,
+    SideComponent
   ],
   imports: [
     CommonModule,
---
+++ src/app/pages/profile/side/side.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-side',
+  templateUrl: './side.component.html',
+  styleUrls: ['./side.component.scss']
+})
+export class SideComponent {
+  // sides = [
+  //   {
+  //     title: 'Strong side',
+  //     bg: '#F5F9F9',
+  //     color: '#479696',
+  //     desc: [
+  //       {name: 'Analytics'},
+  //       {name: 'Perfectionism'},
+  //       {name: 'Analytics'}
+  //     ]
+  //   }
+  // ]
+}
